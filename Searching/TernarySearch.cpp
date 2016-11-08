923ba8c86a5eb3f1d564241c04b58db0225ece1c
brankoFu <brankofulurija@gmail.com>
2016-11-08 09:48:31 +0100

    Add files via upload

---
+++ Searching/TernarySearch.cpp
@@ -0,0 +1,36 @@
+// branko_fu (Branko Fulurija)
+// Ternary Search
+// Given a unimodal function f(x) = 2x^2 - 12x + 7, and several intervals,
+// for each interval find the minimum value for f(x) in that range.
+// Time complexity (log3(N))
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+double f(double x){
+    return 2*x*x - 12*x +7;
+}
+
+double ternarySearch(double lo, double hi){
+    for (int i = 0; i < 200; i++){
+        double mid1 = lo + (hi - lo) / 3;
+        double mid2 = hi - (hi - lo) / 3;
+        if (f(mid1) < f(mid2))
+            hi = mid2;
+        else
+            lo = mid1;
+    }
+    double x = lo;
+    return f(lo);
+}
+
+int main(){
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++){
+        int l, r;
+        cin >> l >> r;
+        cout << ternarySearch(l, r) << "\n";
+    }
+}
